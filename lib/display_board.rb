c322bfbebcfec55bf8a8bc5f157189564a6c30d5
Sherief Farid <sherief@farid.cloud>
2020-01-08 21:52:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,18 @@
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board (board)
 
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts"-----------"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts"-----------"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+>>>>>>> ff0886ef1f23a2722b0f3983643ee65e0eeb33b2
 
 end
